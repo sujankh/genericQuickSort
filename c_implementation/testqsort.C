666900f09a9f52b60338b9a507d799307981c595
Sujan Khadka <sujank@uab.edu>
2016-03-01 01:07:32 -0600

    Updates to sort a double array

--- c_implementation/testqsort.C
+++ c_implementation/testqsort.C
@@ -1,6 +1,4 @@
 #include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
 
 #include "quicksort.h"
 
@@ -27,15 +25,44 @@ void printInt(void *num)
   printf("%d", *(int*)num);
 }
 
+
+int compareDouble(void *n1, void *n2)
+{
+  double num1 = *(double*)n1,
+    num2 = *(double*)n2;
+
+  return num1 - num2;
+}
+
+const size_t doubleSize()
+{
+  return sizeof(double);
+}
+
+void printDouble(void *num)
+{
+  printf("%lf", *(double*)num);
+}
+
 int main()
-{ 
+{
+  double d[] = { 6.5, 10.4, 13.22, 5.54, 8.6, 3.5, 2.9, 11.3 };
+ 
   int a[] = { 6, 10, 13, 5, 8, 3, 2, 11 };
   compare = compareInt; //Our comparator function to compare two ints
   getSize = intSize;
   printElement = printInt;
   
-  quicksort(a, 0, 7);
-  
+  quicksort(a, 0, 7);  
   printArray(a, 7);
+
+  //Now lets sort a double array
+  compare = compareDouble; //Our comparator function to compare two ints
+  getSize = doubleSize;
+  printElement = printDouble;
+  
+  quicksort(d, 0, 7);  
+  printArray(d, 7);
+  
   return 0;
 }
